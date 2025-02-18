051f4b37e45282e280919562c4cb85ab5dc3cd3c
Ekaterina_Frolova1 <kate11610@gmail.com>
2025-02-18 21:21:27 +0300

    Day 1 commit

---
+++ Beginner/Day1/D1_Final.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator")
+city = input("Type your city of birth: \n")
+pet = input("Type your pet's name or create one: \n")
+print("Your Band's name is " + city + " " + pet)
